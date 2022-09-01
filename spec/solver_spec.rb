e5252bce0027f9997c5a73cc10b708009e03ca2d
maen <maenkh@yahoo.com>
2022-09-01 22:56:40 +0300

    create TDD for reverse function

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,5 +6,13 @@ describe "Solver" do
             expect(number.Factorial(0)).to eq(1)
             expect(number.Factorial()).to eq(1)
         end
-    end    
+    end   
+    context "reverse" do
+        it "return the the reverse of the input string" do
+            number = Solver.new
+            expect(number.Factorial('hello')).to eq('olleh')
+            expect(number.Factorial('Maen')).to eq('neaM')
+            expect(number.Factorial('conding')).to eq('gnidnoc')
+        end
+    end   
 end
